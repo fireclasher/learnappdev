4f5e431ee24966cdb62b3a625ef21ac33d85c2a2
fireclasher <akileswar2002@protonmail.com>
2021-04-21 17:23:53 +0530

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "hello world",
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                fontFamily: 'Indie'),
-          ),
-          backgroundColor: Colors.amber,
-          centerTitle: true,
+void main() => runApp(MaterialApp(home: home()));
+
+class home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "hello world",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              fontFamily: 'Indie'),
         ),
-        body: Center(
-          child: Text(
-            "hello world!!!",
-            style: TextStyle(
-                fontWeight: FontWeight.w900, fontFamily: 'Indie', fontSize: 40),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Hey!"),
-          backgroundColor: Colors.amber,
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "hello world!!!",
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontFamily: 'Indie', fontSize: 40),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Hey!"),
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
