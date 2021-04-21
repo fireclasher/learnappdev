37c83bab6282dbf43abb0edb1ad4a66615d6d357
fireclasher <akileswar2002@protonmail.com>
2021-04-21 17:09:15 +0530

    colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("hello world"),
+          title: Text(
+            "hello world",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                fontFamily: 'Indie'),
+          ),
+          backgroundColor: Colors.amber,
           centerTitle: true,
         ),
         body: Center(
-          child: Text("hello world!!!"),
+          child: Text(
+            "hello world!!!",
+            style: TextStyle(
+                fontWeight: FontWeight.w900, fontFamily: 'Indie', fontSize: 40),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text("Hey!"),
+          backgroundColor: Colors.amber,
         ),
       ),
     ));
