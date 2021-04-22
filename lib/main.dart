7dad01398c7db08048f6d852b2f13203a0832b13
fireclasher <akileswar2002@protonmail.com>
2021-04-22 07:31:09 +0530

    network image

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,8 @@ class home extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          "hello world!!!",
-          style: TextStyle(
-              color: Colors.deepOrange,
-              fontWeight: FontWeight.w900,
-              fontFamily: 'Indie',
-              fontSize: 40),
-        ),
+        child: Image.network(
+            "https://images.unsplash.com/photo-1611505700922-57892ff55bbb?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
